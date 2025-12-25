1eed7becc9e7310bd4e058c7797e8735c72f18f8
1317998146-arch <1317998146@qq.com>
2025-12-25 19:30:42 +0800

    Create  01_data_exploration.sql
    Add initial SQL exploration structure

---
+++ sql/01_data_exploration.sql
@@ -0,0 +1,9 @@
+/*
+Project: customer-service-insights
+Purpose: Initial data exploration for customer service data
+Author: ZJY
+*/
+
+-- TODO: Define table structure
+-- TODO: Basic SELECT queries
+-- TODO: Identify key metrics (volume, response time, resolution rate)
